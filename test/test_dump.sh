64157fc72d8fae5a1d8ab4d8df4d3798c177016e
jack.tang <jack.tang@netis.com.cn>
2018-07-19 16:08:16 +0800

    add network interface parameter for test on Mac

--- test/test_dump.sh
+++ test/test_dump.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+NIC=$1
+if [ ! $1 ]; then
+    NIC='eth0'
+fi
+
 rm -rf gredump.pcap
 rm -rf pktminer_dump.pcap
 
-tcpreplay --pps=50 -i eth0 xml.pcap &
+tcpreplay --pps=50 -i $NIC xml.pcap &
 
-../bin/gredump -i eth0 -o gredump.pcap &
-../bin/pktminerg -i eth0 -r 172.16.14.249 -c 1000 --dump
+../bin/gredump -i $NIC -o gredump.pcap &
+../bin/pktminerg -i $NIC -r 172.16.14.249 -c 1000 --dump
 
 sleep 3
 
