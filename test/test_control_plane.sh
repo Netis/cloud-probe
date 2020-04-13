1baadd9e707cd913217e2354bc64409ecbfb37de
Justin.Su <Justin.Su@netis.com>
2020-04-13 14:04:03 +0800

    add zmq test

---
+++ test/test_control_plane.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NIC=$1
+if [ ! $1 ]; then
+    NIC='eth0'
+fi
+
+../bin/pktminerg -i $NIC -r 172.16.14.249 --control 5556 &
+sleep 10
+python test_control_plane_client.py
+sleep 10
+killall pktminerg
+
