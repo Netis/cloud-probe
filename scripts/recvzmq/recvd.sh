f9c40ddf0ad62b5d01cfe95220e3714ae34858e3
jack.tang <jack.tang@netis.com.cn>
2020-04-26 16:00:39 +0800

    install pyzmq rpms offline

--- scripts/recvzmq/recvd.sh
+++ scripts/recvzmq/recvd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p /opt/bpc/var/log
 python /usr/local/bin/recvzmq.py -z 82 -s 15 -a 4 -t /opt/pcap_cache/nic0/%Y%m%d%H%M%S 1>>/opt/bpc/var/log/recvzmq.log 2>>/opt/bpc/var/log/recvzmq.err &
 RECV_PID=$! 
 
@@ -6,6 +7,7 @@ rm -rf /var/run/recvzmq/recvzmq.pid
 sh /usr/local/bin/backup_recvlog.sh /opt/bpc/var/log/recvzmq.log &
 BACKUP_PID=$!
 
+mkdir -p /var/run/recvzmq
 echo "$RECV_PID" > /var/run/recvzmq/recvzmq.pid
 echo "$BACKUP_PID" >> /var/run/recvzmq/recvzmq.pid
 
--- scripts/recvzmq_installer.sh
+++ scripts/recvzmq_installer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 RECV_TAR_GZ=$1
+MAJOR_VERSION=0
+if [ -e /etc/centos-release ]; then
+    MAJOR_VERSION=`rpm -q --queryformat '%{VERSION}' centos-release`
+fi
 
 Usage() {
     echo "Usage:"
@@ -16,17 +20,43 @@ if [[ -z "$RECV_TAR_GZ" ]]; then
     exit 1
 fi
 
-yum -y install python-zmq
+
+install_recvzmq() {
+    \cp -rf recvzmq_installer/recvzmq.py /usr/local/bin/recvzmq.py
+    \cp -rf recvzmq_installer/backup_recvlog.sh /usr/local/bin/backup_recvlog.sh
+    \cp -rf recvzmq_installer/recvd.sh /usr/local/etc/recvd.sh
+    if [ $MAJOR_VERSION = "6" ]; then
+        echo "sh /usr/local/etc/recvd.sh &" >> /etc/rc.d/rc.local
+    elif [ $MAJOR_VERSION = "7" ]; then
+        \cp -rf recvzmq_installer/recvzmq.service /etc/systemd/system/recvzmq.service
+    fi
+
+    echo "Install finished"
+}
+
+
+install_pyzmq() {
+    if [ $MAJOR_VERSION = "6" ]; then
+        rpm -ivh recvzmq_installer/pyzmq-el6/openpgm-5.1.118-3.el6.x86_64.rpm
+        rpm -ivh recvzmq_installer/pyzmq-el6/zeromq3-3.2.5-1.el6.x86_64.rpm
+        rpm -ivh recvzmq_installer/pyzmq-el6/python-zmq-14.3.1-1.el6.x86_64.rpm
+    elif [ $MAJOR_VERSION = "7" ]; then
+        rpm -ivh recvzmq_installer/pyzmq-el7/libsodium-1.0.18-1.el7.x86_64.rpm
+        rpm -ivh recvzmq_installer/pyzmq-el7/openpgm-5.2.122-2.el7.x86_64.rpm
+        rpm -ivh recvzmq_installer/pyzmq-el7/zeromq-4.1.4-6.el7.x86_64.rpm
+        rpm -ivh recvzmq_installer/pyzmq-el7/python2-zmq-14.7.0-11.el7.x86_64.rpm
+    else
+        echo "Unsupported version: $MAJOR_VERSION"
+        exit 1
+    fi
+}
+
 
 mkdir -p recvzmq_installer
 tar -xvf $RECV_TAR_GZ --strip-components 1 -C recvzmq_installer/
 
-\cp -rf recvzmq_installer/recvzmq.py /usr/local/bin/recvzmq.py
-\cp -rf recvzmq_installer/backup_recvlog.sh /usr/local/bin/backup_recvlog.sh
-\cp -rf recvzmq_installer/recvd.sh /usr/local/etc/recvd.sh
-\cp -rf recvzmq_installer/recvzmq.service /etc/systemd/system/recvzmq.service
+install_pyzmq
+install_recvzmq
 
 rm -rf recvzmq_installer
-
-echo "Install finished"
 
